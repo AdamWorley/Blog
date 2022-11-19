35acd97e8d7a2389421801e1c0b3a465a8682f58
Adam Worley <28906234+AdamWorley@users.noreply.github.com>
2022-11-19 17:07:44 +0000

    change theme toggle

--- src/components/ThemeToggleButton.svelte
+++ src/components/ThemeToggleButton.svelte
@@ -1,65 +1,107 @@
 <script>
-  const rootEl = typeof document !== 'undefined' ? document.documentElement : null;
-  const themes = ['light', 'dark'];
-  let theme = ''
+  const rootEl =
+    typeof document !== "undefined" ? document.documentElement : null;
 
-  if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
-    theme = localStorage.getItem('theme');
-  } else if (typeof window !== 'undefined' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    theme = 'dark';
+  let darkModeEnabled = false;
+
+  if (typeof localStorage !== "undefined" && localStorage.getItem("darkModeEnabled")) {
+    darkModeEnabled = localStorage.getItem("darkModeEnabled");
+  } else if (
+    typeof window !== "undefined" &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    darkModeEnabled = true;
   }
 
   function handleChange(event) {
-    theme = event.target.value;
-    localStorage.setItem('theme', theme);
+    darkModeEnabled = event.target.checked;
+    localStorage.setItem("darkModeEnabled", darkModeEnabled);
   }
 
-  $: if (rootEl && theme === 'light') {
-    rootEl.classList.remove('theme-dark');
-  } else if (rootEl && theme === 'dark') {
-    rootEl.classList.add('theme-dark');
+  $: if (rootEl && !darkModeEnabled) {
+    rootEl.classList.remove("theme-dark");
+  } else if (rootEl && darkModeEnabled) {
+    rootEl.classList.add("theme-dark");
   }
-
-  const icons = [
-    `<svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-        clip-rule="evenodd"
-      />
-    </svg>`,
-    `<svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-    </svg>`,
-  ];
 </script>
 
-
 <div class="theme-toggle">
-  {#each themes as t, i}
-    <label class={theme === t ? 'checked' : ''}>
-      {@html icons[i]}
-      <input
-        type="radio"
-        name="theme-toggle"
-        checked={theme === t}
-        value={t}
-        title={`Use ${t} theme`}
-        aria-label={`Use ${t} theme`}
-        on:change={handleChange}
-      />
-    </label>
-  {/each}
+  <label class="switch">
+    <input
+      id="theme-checkbox"
+      type="checkbox"
+      name="theme-toggle"
+      checked={darkModeEnabled}
+      title="Enable dark theme"
+      aria-label="Enable dark theme"
+      on:change={handleChange}
+    />
+    <span class="slider" />
+  </label>
 </div>
+
+<style>
+  .theme-toggle input[type="checkbox"] {
+    visibility: hidden;
+    display: none;
+  }
+
+  .theme-toggle *,
+  .theme-toggle ::after,
+  .theme-toggle ::before {
+    box-sizing: border-box;
+  }
+
+  /* The switch - the box around the slider */
+  .theme-toggle .switch {
+    --width-of-switch: 3.5em;
+    --height-of-switch: 2em;
+    /* size of sliding icon -- sun and moon */
+    --size-of-icon: 1.4em;
+    /* it is like a inline-padding of switch */
+    --slider-offset: 0.3em;
+    position: relative;
+    width: var(--width-of-switch);
+    height: var(--height-of-switch);
+    display: inline-block;
+  }
+
+  /* The slider */
+  .theme-toggle .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #f4f4f5;
+    transition: 0.4s;
+    border-radius: 30px;
+  }
+
+  .theme-toggle .slider:before {
+    position: absolute;
+    content: "";
+    height: var(--size-of-icon, 1.4em);
+    width: var(--size-of-icon, 1.4em);
+    border-radius: 20px;
+    left: var(--slider-offset, 0.3em);
+    top: 50%;
+    transform: translateY(-50%);
+    background: linear-gradient(40deg, #ff0080, #ff8c00 70%);
+    transition: 0.4s;
+  }
+
+  .theme-toggle input:checked + .slider {
+    background-color: #303136;
+  }
+
+  .theme-toggle input:checked + .slider:before {
+    left: calc(
+      100% - (var(--size-of-icon, 1.4em) + var(--slider-offset, 0.3em))
+    );
+    background: #303136;
+    /* change the value of second inset in box-shadow to change the angle and direction of the moon  */
+    box-shadow: inset -3px -2px 5px -2px #8983f7, inset -10px -4px 0 0 #a3dafb;
+  }
+</style>
