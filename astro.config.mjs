db4f174dce0cb9ddf007e59d628884508cd679c5
Adam Worley <adam.worley@netwealth.com>
2023-09-01 10:07:04 +0100

    feat: use blog collection
    - mdx now works

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,25 @@
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
-import rehypeExternalLinks from 'rehype-external-links';
+import rehypeExternalLinks from "rehype-external-links";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.adamworley.com/",
   integrations: [mdx(), svelte()],
   markdown: {
+    remarkPlugins: [remarkReadingTime],
     shikiConfig: {
       theme: "nord",
     },
     rehypePlugins: [
-      [rehypeExternalLinks,
-      {
-        target: "_blank",
-      }],
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+        },
+      ],
     ],
   },
 });
